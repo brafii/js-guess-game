a312f18ca9a7bb4e9962e06cfb3dd15be2d7c6a8
Fiifi Ofori <fiifi56@gmail.com>
2021-05-11 16:33:52 +0000

    Initial commit

---
+++ js/model.js
@@ -0,0 +1,11 @@
+
+
+document.querySelector('.guess-play').addEventListener('click', function(){
+
+    if(!guess-play){
+        document.querySelector('.result-info').textContent = 'Invalid Input';
+    }
+
+
+
+});

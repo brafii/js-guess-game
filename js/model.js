407226b0eae8205738bf28ce798d10d55f9de824
Fiifi Ofori <fiifi56@gmail.com>
2021-06-01 14:22:31 +0000

    Complete App

--- js/model.js
+++ js/model.js
@@ -1,9 +1,49 @@
 
+let changeGame = Math.trunc(Math.random() * 10) + 1;
+let counter = 10;
+
+// document.querySelector('.guessing').textContent = changeGame;
 
 document.querySelector('.guess-play').addEventListener('click', function(){
 
-    if(!guess-play){
-        document.querySelector('.result-info').textContent = 'Invalid Input';
+    let userInput = Number(document.querySelector('.user-input').value);
+
+
+    if(!userInput){
+        document.querySelector('.result-info').textContent = 'Invalid Input! Try again';
+    }
+    else if(userInput > changeGame){
+
+        if(counter > 1){
+            document.querySelector('.result-info').textContent = 'Too High! Try again';
+            counter--;
+            document.querySelector('.counter').textContent = counter;
+            document.querySelector('.counter1').textContent = counter;
+        }else{
+            document.querySelector('.result-info').textContent = '🔥🔥🔥 Game Over';
+            document.querySelector('.counter').textContent = 0;
+            document.querySelector('.counter1').textContent = 0;
+        }
+
+        
+    }
+    else if(userInput < changeGame){
+
+        if(counter > 1){
+            document.querySelector('.result-info').textContent = 'Too Low! Try again';
+            counter--;
+            document.querySelector('.counter').textContent = counter;
+            document.querySelector('.counter1').textContent = counter;
+        }else{
+            document.querySelector('.result-info').textContent = '🔥🔥🔥 Game Over';
+            document.querySelector('.counter').textContent = 0;
+            document.querySelector('.counter1').textContent = 0;
+        }
+        
+    }
+    else if(userInput === changeGame){
+        document.querySelector('.result-info').textContent = '🎉🎉🎉 You Win';
+        document.querySelector('.result').style.backgroundColor =  '#00b300';
     }
 
 
